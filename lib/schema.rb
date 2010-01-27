672303667c937fec1ed7f3069b93490badd5da36
Peter Suschlik <peter@suschlik.de>
2010-01-27 22:11:18 +0100

    Refactoring and implement nesting.

--- lib/schema.rb
+++ lib/schema.rb
@@ -1,29 +1,100 @@
+require 'pp'
+
 class Schema
+
+  class Validator
+    attr_reader :validators
+
+    def initialize(schema, &block)
+      @schema = schema
+      @validators = [ block ]
+    end
+
+    def method_missing(method, *args, &block)
+      @validators.concat(@schema.send(method, *args, &block).validators)
+      self
+    end
+
+    def validate(value)
+      @validators.all? do |block|
+        block.call(value)
+      end
+    end
+
+    def size
+      @validators.size
+    end
+
+    def inspect
+      "#<%s:%x @validators=%d>" % [ self.class.name, hash, size ]
+    end
+  end
+
+  # Schema
+  attr_reader :validators
+
+  class << self
+    attr_reader :matchers
+  end
+  @matchers = {}
+
+  def self.validate(value, &block)
+    Schema.new(&block).validate(value)
+  end
+
   def initialize(&block)
     @validators = []
     instance_eval(&block) if block
   end
 
-  def equals(expected)
-    append { |value| value == expected }
+  def self.matcher(name, &block)
+    @matchers[name] = proc do |value, expected|
+      result = block.call(value, expected)
+      puts "#{value.inspect} #{name} #{expected.inspect} => #{result.inspect}"
+      result
+    end
   end
 
-  def is_a?(expected)
-    append { |value| value.is_a?(expected) }
+  matcher(:equals) { |value, expected| value == expected }
+  matcher(:is_a?) { |value, expected| value.is_a?(expected) }
+
+  matchers.keys.each { |m| undef_method m if respond_to?(m) }
+
+  def value
+    Schema.new
   end
 
-  def validate(value = nil)
-    if @validators.empty?
-      value.nil?
-    else
-      @validators.all? do |validator|
-        validator.call(value)
+  def hash(schema={})
+    valid? do |value|
+      schema.all? do |(key, validator)|
+        validator.validate(value[key])
       end
     end
   end
 
-  def append(&block)
-    @validators << block
-    self
+  def validate!(value = nil)
+    if @validators.empty?
+      value.nil?
+    else
+      @validators.all? do |validator|
+        validator.validate(value)
+      end
+    end
+  end
+
+  def valid?(&block)
+    Validator.new(self, &block).tap do |validator|
+      @validators << validator
+    end
+  end
+
+  def method_missing(method, *args, &block)
+    if matcher = self.class.matchers[method]
+      valid? do |value|
+        matcher.call(value, args.first)
+      end
+    else
+      super
+    end
   end
 end
--- test/test_schema.rb
+++ test/test_schema.rb
@@ -2,7 +2,7 @@ require 'helper'
 
 class Riot::Situation
   def v(value=nil, &block)
-    Schema.new(&block).validate(value)
+    Schema.new(&block).validate!(value)
   end
 end
 
@@ -16,9 +16,11 @@ context "Schema" do
     asserts("validates 1") { !v(1) }
   end
 
-  context "chaining" do
-    asserts("valid") { v(1) { is_a?(Fixnum).equals(1) }}
-    asserts("invalid") { !v(1) { is_a?(Fixnum).equals(2) }}
+  context "matchers" do
+    asserts("equals") { Schema.matchers[:equals].call(1, 1) }
+    asserts("!equals") { !Schema.matchers[:equals].call(1, 2) }
+    asserts("is_a?") { Schema.matchers[:is_a?].call("string", String) }
+    asserts("!is_a?") { !Schema.matchers[:is_a?].call("string", Fixnum) }
   end
 
   context "equals" do
@@ -29,10 +31,31 @@ context "Schema" do
     asserts("invalid multiple") { !v(1) {equals 1; equals 2} }
   end
 
+
   context "is_a?" do
     asserts("valid String") { v("test") { is_a?(String) } }
     asserts("invalid String") { !v(1) { is_a?(String) } }
-    asserts("valid multple") { v("test") { is_a?(String); is_a?(Object) } }
-    asserts("invalid multple") { !v("test") { is_a?(String); is_a?(Fixnum) } }
+    asserts("valid multiple") { v("test") { is_a?(String); is_a?(Object) } }
+    asserts("invalid multiple") { !v("test") { is_a?(String); is_a?(Fixnum) } }
+  end
+
+  context "chaining" do
+    asserts("valid") { v(1) { is_a?(Fixnum).equals(1) }}
+    asserts("invalid") { !v(1) { is_a?(Fixnum).equals(2) }}
+  end
+
+  context "hash" do
+    setup do
+      Schema.new do
+        hash(
+          :key    => value.is_a?(String),
+          :value  => value.is_a?(Fixnum)
+        )
+      end
+    end
+
+    asserts("valid") { topic.validate!(:key => "fasel", :value => 23) }
+    asserts("invalid") { !topic.validate!(:key => 23, :value => 23) }
+    asserts("missing") { !topic.validate!(:key => "fasel") }
   end
 end
